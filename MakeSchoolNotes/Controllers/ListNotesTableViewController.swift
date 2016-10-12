733f698220908243c1966d906f92147ca65ef218
Michael <michaelloubier@michaels-macbook-pro.local>
2016-10-12 12:21:08 -0700

    Worked on the project during class. Made it to chapter 7 of the tutorial.

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -14,4 +14,25 @@ class ListNotesTableViewController: UITableViewController {
         super.viewDidLoad()
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        
+        cell.noteTitleLabel.text = "note's title"
+        cell.noteModificationTimeLabel.text = "note's modification time"
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "displayNote" {
+                print("Transitioning to the Display Note View Controller")
+            }
+        }
+    }
+    
 }
